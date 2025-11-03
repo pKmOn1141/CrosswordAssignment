009eaddccac594224eb60f5a4b4d4f1d94d6f15d
James <clubjec@gmail.com>
2025-11-03 23:13:08 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrosswordAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this should be on github");
+        }
+    }
+}
